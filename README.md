83e83817b2fbabf9e35d279a5f63519c759dd235
pozil <philippe.ozil@gmail.com>
2022-01-10 15:03:03 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,129 @@
+# Node gRPC client for the Salesforce Pub/Sub API
+
+**Warning:** as of the Winter '22 release, the Pub/Sub API is currently in pilot. The implementation of this project may change once the features are GA.
+
+This project is derived from a [blog post](https://jungleeforce.com/2021/11/11/connecting-to-salesforce-using-pub-sub-api-grpc/) written by techinjungle.
+
+See the [official Pub/Sub API pilot repo](https://github.com/developerforce/pub-sub-api-pilot) for more information on the Pub/Sub API.
+
+## Installation
+
+Create a `.env` file at the root of the project:
+
+```properties
+SALESFORCE_LOGIN_URL=https://login.salesforce.com
+SALESFORCE_USERNAME=YOUR_SALESFORCE_USERNAME
+SALESFORCE_PASSWORD=YOUR_SALESFORCE_PASSWORD
+
+PUB_SUB_ENDPOINT=api.pilot.pubsub.salesforce.com:7443
+PROTO_FILE=pubsub_api.proto
+
+TOPIC_NAME=/data/AccountChangeEvent
+EVENT_RECEIVE_LIMIT=1
+```
+
+> **Warning:** this project relies on a username/password Salesforce authentication flow. This is only recommended for test purposes. Consider switching to JWT auth for extra security.
+
+If using a Change Data Capture topic (like in the sample config), make sure to activate the event in Salesforce Setup > Change Data Capture.
+
+## Execution
+
+Run the project with `npm start`
+
+If everything goes well, you'll see output like this:
+
+```
+Connected to Salesforce org 00D7Q000002DBrqUAG: https://pozilcom-dev-ed.my.salesforce.com
+Connected to Pub/Sub API
+Topic schema loaded: /data/AccountChangeEvent
+Pub/Sub subscribe request sent for 1 events...
+```
+
+At this point the script will be on hold and will wait for events.
+Once it receives events, it will display them like this:
+
+```
+Received 1 events, latest replay ID: 193945
+gRPC event payloads:  [
+  {
+    "replayId": "193945",
+    "payload": {
+      "ChangeEventHeader": {
+        "entityName": "Account",
+        "recordIds": [
+          "0017Q000005yIARQA2"
+        ],
+        "changeType": "UPDATE",
+        "changeOrigin": "com/salesforce/api/soap/53.0;client=SfdcInternalAPI/",
+        "transactionKey": "0006d2d3-f3c2-4bde-b75a-8e21f1acc21c",
+        "sequenceNumber": 1,
+        "commitTimestamp": 1641819688000,
+        "commitNumber": 156839803268,
+        "commitUser": "0057Q000002aGVkQAM",
+        "nulledFields": [],
+        "diffFields": [],
+        "changedFields": [
+          "0x420000"
+        ]
+      },
+      "Name": null,
+      "Type": null,
+      "ParentId": null,
+      "BillingAddress": null,
+      "ShippingAddress": null,
+      "Phone": null,
+      "Fax": null,
+      "AccountNumber": null,
+      "Website": null,
+      "Sic": null,
+      "Industry": null,
+      "AnnualRevenue": null,
+      "NumberOfEmployees": null,
+      "Ownership": null,
+      "TickerSymbol": null,
+      "Description": null,
+      "Rating": {
+        "string": "Hot"
+      },
+      "Site": null,
+      "OwnerId": null,
+      "CreatedDate": null,
+      "CreatedById": null,
+      "LastModifiedDate": {
+        "long": 1641819688000
+      },
+      "LastModifiedById": null,
+      "Jigsaw": null,
+      "JigsawCompanyId": null,
+      "CleanStatus": null,
+      "AccountSource": null,
+      "DunsNumber": null,
+      "Tradestyle": null,
+      "NaicsCode": null,
+      "NaicsDesc": null,
+      "YearStarted": null,
+      "SicDesc": null,
+      "DandbCompanyId": null,
+      "OperatingHoursId": null,
+      "CustomerPriority__c": null,
+      "SLA__c": null,
+      "Active__c": null,
+      "NumberofLocations__c": null,
+      "UpsellOpportunity__c": null,
+      "SLASerialNumber__c": null,
+      "SLAExpirationDate__c": null
+    }
+  }
+]
+```
+
+After receiving the number of requested events (see `EVENT_RECEIVE_LIMIT`), the script will terminate with these messages:
+
+```
+gRPC stream status:  {
+  code: 0,
+  details: '',
+  metadata: Metadata { _internal_repr: {}, flags: 0 }
+}
+gRPC stream ended
+```
---
+++ index.js
@@ -0,0 +1,212 @@
+// This project is derived from a [blog post](https://jungleeforce.com/2021/11/11/connecting-to-salesforce-using-pub-sub-api-grpc/) from techinjungle.
+// Official Pub/Sub API pilot repo: https://github.com/developerforce/pub-sub-api-pilot
+
+const grpc = require('grpc');
+const fs = require('fs');
+const jsforce = require('jsforce');
+const avro = require('avro-js');
+const certifi = require('certifi');
+
+// Load config from .env file
+require('dotenv').config();
+const {
+    SALESFORCE_LOGIN_URL,
+    SALESFORCE_USERNAME,
+    SALESFORCE_PASSWORD,
+    PUB_SUB_ENDPOINT,
+    PROTO_FILE,
+    TOPIC_NAME,
+    EVENT_RECEIVE_LIMIT
+} = process.env;
+
+// Load GRPC
+const protoLoader = require('@grpc/proto-loader');
+const packageDef = protoLoader.loadSync(PROTO_FILE, {});
+const grpcObj = grpc.loadPackageDefinition(packageDef);
+const sfdcPackage = grpcObj.eventbus.v1;
+
+/**
+ * Connects to Salesforce using jsForce
+ * This is required to obtain an access token that the Pub/Sub API uses.
+ * @returns the connection information required by the Pub/Sub API
+ */
+async function connectToSalesforce() {
+    const conn = new jsforce.Connection({
+        loginUrl: SALESFORCE_LOGIN_URL
+    });
+    const loginResult = await conn.login(
+        SALESFORCE_USERNAME,
+        SALESFORCE_PASSWORD
+    );
+    console.log(
+        `Connected to Salesforce org ${loginResult.organizationId}: ${conn.instanceUrl}`
+    );
+    return {
+        organizationId: loginResult.organizationId,
+        instanceUrl: conn.instanceUrl,
+        accessToken: conn.accessToken
+    };
+}
+
+/**
+ * Connects to the Pub/Sub API and returns a gRPC client
+ * @param {Object} connectionInfo the connection information required by the Pub/Sub API
+ * @param {string} connectionInfo.organizationId
+ * @param {string} connectionInfo.instanceUrl
+ * @param {string} connectionInfo.accessToken
+ * @returns a gRPC client
+ */
+function connectToPubSubApi(connectionInfo) {
+    const metaCallback = (_params, callback) => {
+        const meta = new grpc.Metadata();
+        meta.add('accesstoken', connectionInfo.accessToken);
+        meta.add('instanceurl', connectionInfo.instanceUrl);
+        meta.add('tenantid', connectionInfo.organizationId);
+        callback(null, meta);
+    };
+
+    const rootCert = fs.readFileSync(certifi);
+
+    const callCreds =
+        grpc.credentials.createFromMetadataGenerator(metaCallback);
+    const combCreds = grpc.credentials.combineChannelCredentials(
+        grpc.credentials.createSsl(rootCert),
+        callCreds
+    );
+
+    const client = new sfdcPackage.PubSub(PUB_SUB_ENDPOINT, combCreds);
+    console.log(`Connected to Pub/Sub API`);
+    return client;
+}
+
+/**
+ * Requests the event schema for a topic
+ * @param {Object} client Pub/Sub API gRPC client
+ * @param {string} topicName name of the topic that we're fetching
+ * @returns {Object} parsed event schema `{id: string, type: Object}`
+ */
+async function getEventSchema(client, topicName) {
+    return new Promise((resolve, reject) => {
+        client.GetTopic({ topicName }, (err, response) => {
+            if (err) {
+                // Handle error
+                reject(err);
+            } else {
+                //get the schema information
+                const schemaId = response.schemaId;
+                client.GetSchema({ schemaId }, (error, res) => {
+                    if (error) {
+                        // Handle error
+                        reject(err);
+                    } else {
+                        const schemaType = avro.parse(res.schemaJson);
+                        console.log(`Topic schema loaded: ${topicName}`);
+                        resolve({
+                            id: schemaId,
+                            type: schemaType
+                        });
+                    }
+                });
+            }
+        });
+    });
+}
+
+/**
+ * Subscribes to a topic using the gRPC client and an event schema
+ * @param {Object} client Pub/Sub API gRPC client
+ * @param {string} topicName name of the topic that we're subscribing to
+ * @param {Object} schema event schema associated with the topic
+ * @param {string} schema.id
+ * @param {Object} schema.type
+ */
+function subscribe(client, topicName, schema) {
+    const subscription = client.Subscribe(); //client here is the grpc client.
+    //Since this is a stream, you can call the write method multiple times.
+    //Only the required data is being passed here, the topic name & the numReqested
+    //Once the system has received the events == to numReqested then the stream will end.
+    const subscribeRequest = {
+        topicName,
+        numRequested: EVENT_RECEIVE_LIMIT
+    };
+    subscription.write(subscribeRequest);
+    console.log(
+        `Pub/Sub subscribe request sent for ${subscribeRequest.numRequested} events...`
+    );
+
+    // Listen to new events.
+    subscription.on('data', (data) => {
+        if (data.events) {
+            const latestReplayId = data.latestReplayId.readBigUInt64BE();
+            console.log(
+                `Received ${data.events.length} events, latest replay ID: ${latestReplayId}`
+            );
+            const parsedEvents = data.events.map((event) => {
+                const replayId = event.replayId.readBigUInt64BE().toString();
+                const payload = schema.type.fromBuffer(event.event.payload); // This schema is the same which we retreived earlier in the GetSchema rpc.
+                return {
+                    replayId,
+                    payload
+                };
+            });
+            console.log(
+                'gRPC event payloads: ',
+                JSON.stringify(parsedEvents, null, 2)
+            );
+        } else {
+            // If there are no events then every 270 seconds the system will keep publishing the latestReplayId.
+        }
+    });
+    subscription.on('end', () => {
+        console.log('gRPC stream ended');
+    });
+    subscription.on('error', (err) => {
+        // Handle errors
+        console.error('gRPC stream error: ', JSON.stringify(err));
+    });
+    subscription.on('status', (status) => {
+        console.log('gRPC stream status: ', status);
+    });
+}
+
+/**
+ * Publishes a payload to a topic using the gRPC client
+ * @param {Object} client Pub/Sub API gRPC client
+ * @param {string} topicName name of the topic that we're subscribing to
+ * @param {Object} schema event schema associated with the topic
+ * @param {string} schema.id
+ * @param {Object} schema.type
+ * @param {Object} payload
+ */
+async function publish(client, topicName, schema, payload) {
+    return new Promise((resolve, reject) => {
+        client.Publish(
+            {
+                topicName,
+                events: [
+                    {
+                        id: '124', // this can be any string
+                        schemaId: schema.id,
+                        payload: schema.type.toBuffer(payload)
+                    }
+                ]
+            },
+            (err, response) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(response);
+                }
+            }
+        );
+    });
+}
+
+async function run() {
+    const connectionInfo = await connectToSalesforce();
+    const client = connectToPubSubApi(connectionInfo);
+    const topicSchema = await getEventSchema(client, TOPIC_NAME);
+    subscribe(client, TOPIC_NAME, topicSchema);
+}
+
+run();
